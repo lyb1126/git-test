6dcc565d76d8d52990a32a348b8872be7463b03b
lyb1126 <90846581+lyb1126@users.noreply.github.com>
2021-09-20 10:00:15 +0800

    Update GitTest.java

--- src/main/java/com/yc/GitTest.java
+++ src/main/java/com/yc/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master hello 5");
         System.out.println("hot-fix hello 5 ");
         System.out.println("push test ");
+        System.out,println("pull test");
     }
 }
